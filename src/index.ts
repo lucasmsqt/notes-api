24e8594e8f5c06b2566184c0a37ff1c36ce734ba
lucasmsqt <lucacsk3@gmail.com>
2024-12-28 17:04:53 -0300

    feat: adiciona rota /contas

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import authRoutes from './routes/authRoutes';
+import contasRoutes from './routes/contasRoutes'
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -10,6 +11,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 app.use('/auth', authRoutes);
+app.use('/contas', contasRoutes)
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando em http://localhost:${PORT}`);
