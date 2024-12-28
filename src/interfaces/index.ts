2ffbfcc1df52889fc81893297a7751e5d769d9ed
lucasmsqt <lucacsk3@gmail.com>
2024-12-28 17:04:43 -0300

    feat: cria interface CustomJwtPayload

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,6 +1,12 @@
+import { JwtPayload } from "jsonwebtoken";
+
 export interface User {
     id: number;
     email: string;
     password: string;
     created_at: Date;
+}
+
+export interface CustomJwtPayload extends JwtPayload {
+    id: number
 }
