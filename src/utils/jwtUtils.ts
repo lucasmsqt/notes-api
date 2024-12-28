bb9674d111cf82783583fbe363b752a17497dd9c
lucasmsqt <lucacsk3@gmail.com>
2024-12-28 17:03:35 -0300

    feat: cria verifyToken

--- src/utils/jwtUtils.ts
+++ src/utils/jwtUtils.ts
@@ -1,8 +1,18 @@
 import jwt from 'jsonwebtoken'
 import dotenv from 'dotenv'
+import { CustomJwtPayload } from '../interfaces';
 
 dotenv.config();
 
 export function generateToken(payload: object): string {
-    return jwt.sign(payload, String(process.env.JWT_SECRET), {expiresIn: '1h'});
+    return jwt.sign(payload, String(process.env.JWT_SECRET));
+}
+
+export function verifyToken(token: string): CustomJwtPayload {
+    try {
+        const decoded = jwt.verify(token, String(process.env.JWT_SECRET)) as CustomJwtPayload;
+        return decoded;
+    } catch (err) {
+        throw new Error('Token inválido')
+    }
 }
